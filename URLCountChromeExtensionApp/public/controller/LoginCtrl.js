e2cb8482641a4d6687c293747c379686a33677e2
AmitDebadwar <amit.debadwar@gmail.com>
2017-03-21 23:37:38 +0530

    sai

--- URLCountChromeExtensionApp/public/controller/LoginCtrl.js
+++ URLCountChromeExtensionApp/public/controller/LoginCtrl.js
@@ -3,9 +3,23 @@
  */
 
 
-angular.module("UrlCounterApp").controller("LoginCtrl", function ($scope, $state) {
+angular.module("UrlCounterApp").controller("LoginCtrl", function ($scope, $state, $http) {
 
     $scope.login = function () {
-        $state.go('dashboard');
+ 
+        $http({
+            method : "POST",
+            url : "/login",
+            data: {
+                "username":$scope.userName,
+                "password":$scope.password
+            }
+         }).then(function mySucces(response) {
+             $state.go("dashboard");
+        }, function myError(response) {
+             alert("User is not authenticated");
+        });
+
+
     }
 });
